a0b2955e67f683d2b129e3f0b7514e099b34c791
Jesse <jesse.a.heilman@gmail.com>
2015-02-13 15:02:58 +0100

    Jesse's changes to CraneenProcesser to allow for the creation of the data_obs shape in the Higgs Combine shapefile.  Also, renaming of the MuEl XML files to fit new paradigm requiring different XML for selection limit preperation.
    Additionally, an example of MuEl output from CraneedProcesser is uploaded.

--- LaunchParallelMacro.py
+++ LaunchParallelMacro.py
@@ -2,7 +2,7 @@ import xml.etree.cElementTree as ET
 import subprocess
 import time
 import os
-tree = ET.ElementTree(file='config/Run2_Samples.xml')
+tree = ET.ElementTree(file='config/Run2DiLepton_TOPTREES.xml')
 
 root = tree.getroot()
 datasets = root.find('datasets')
