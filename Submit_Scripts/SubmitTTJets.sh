011798ed8265607aced29c44207c184b7cfef059
Lana-B <lana_bk@hotmail.com>
2015-12-11 16:43:37 +0100

    submission script for batch job

--- Submit_Scripts/SubmitTTJets.sh
+++ Submit_Scripts/SubmitTTJets.sh
@@ -9,8 +9,9 @@ echo $TMPDIR
 echo ">> Copying work area"
 cd $TMPDIR/TopBrussels/FourTops
 ls -l
-./SLMACRO TTJets t\bar{t}+jets Madgraph MLM 1 633 1 2 1 3412.1 831.76 0.0 /pnfs/iihe/cms/store/user/lbeck/Skimmed-TopTrees/CMSSW_74X_v8/Muon/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/24112015_184959/Skimmed_TOPTREE_*.root
-echo ">> Training Complete"
+./SLMACRObTag TTJets t\bar{t}+jets Madgraph MLM 1 633 1 2 1 13500.9 831.76 0.0 /pnfs/iihe/cms/store/user/fblekman/TopTree/CMSSW_74X_v8/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/crab_TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8-RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2-CMSSW_74X_v8-MCRUN2_74_V9/151020_160750/0000/TOPTREE_*.root
+echo ">> Complete"
 ls -l
 cp -pr $TMPDIR/TopBrussels/FourTops/Craneens_* /user/lbeck/batchoutput
+cp -pr $TMPDIR/TopBrussels/FourTops/Histo_* /user/lbeck/batchoutput
 cp -pr $TMPDIR/TopBrussels/FourTops/MACRO_* /user/lbeck/batchoutput
